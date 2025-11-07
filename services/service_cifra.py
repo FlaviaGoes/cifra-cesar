ad738360bbe2ab69ad017769e2054df189ec91ad
FlaviaGoes <flav.goes@outlook.com>
2025-11-06 21:25:23 -0300

    refactor: change entrada_valida

--- services/service_cifra.py
+++ services/service_cifra.py
@@ -1,8 +1,11 @@
 from fastapi import HTTPException, status
 from schemas.cifra import Cifrar_Response, Decifrar_Response
+import re
 
 def executar_cifra(texto: str, deslocamento: str) -> Cifrar_Response:
     texto_cifrado = ""
+
+    entrada_valida(texto_claro=texto, deslocamento=deslocamento, mensagemErro="Valide os caracteres utilizados e o número de deslocamento utilizado")
 
     for char in texto:
         if char.isalpha():
@@ -25,9 +28,17 @@ def executar_decifrar(texto_cifrado: str, deslocamento: int) -> Decifrar_Respons
 
     return Decifrar_Response(textoClaro=texto_claro)
 
-def entrada_valida(chave_binario: str, texto_binario: str, mensagemErro: str) -> HTTPException:
-    if len(chave_binario) < len(texto_binario):
+def entrada_valida(texto_claro: str, deslocamento: int, mensagemErro: str) -> HTTPException:
+
+    if(deslocamento <= 0 or deslocamento > 26):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=mensagemErro
         )
+
+    for char in texto_claro:
+        if not re.fullmatch(r"[A-Za-z ]+", char):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"O caractere '{repr(char)}' não é permitido. Use apenas letras sem acento e espaços."
+            )
