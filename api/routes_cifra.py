c8260d423eea625b65ec6745549ee8fa996b399f
RenanYMartins <yohanssen.martins@gmail.com>
2025-11-06 21:24:54 -0300

    :sparkles: part endpoint brute force

--- api/routes_cifra.py
+++ api/routes_cifra.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from schemas.cifra import Cifrar, Cifrar_Response, Decifrar, Decifrar_Response
-from services.service_cifra import executar_cifra, executar_decifrar
+from schemas.cifra import Cifrar, Cifrar_Response, Decifrar, Decifrar_Response, Decifrar_Forca_Bruta, Decifrar_Forca_Bruta_Response
+from services.service_cifra import executar_cifra, executar_decifrar, executar_decifrar_forca_bruta
 
 router = APIRouter(prefix='/cifra', tags=["Cifra"])
 
@@ -11,3 +11,7 @@ def cifrar(input: Cifrar):
 @router.post("/decifrar", response_model=Decifrar_Response)
 def decifrar(input: Decifrar):
     return executar_decifrar(input.texto_cifrado, input.chave)
+
+@router.post("/decifrarForcaBruta", response_model=Decifrar_Forca_Bruta_Response)
+async def decifrarForcaBruta(input: Decifrar_Forca_Bruta):
+    return await executar_decifrar_forca_bruta(input.textoCifrado)
--- services/service_cifra.py
+++ services/service_cifra.py
@@ -1,5 +1,7 @@
 from fastapi import HTTPException, status
-from schemas.cifra import Cifrar_Response, Decifrar_Response
+from schemas.cifra import Cifrar_Response, Decifrar_Response, Decifrar_Forca_Bruta_Response
+import aiohttp
+import re
 
 def executar_cifra(texto: str, chave: str) -> Cifrar_Response:
     texto_cifrado = ""
@@ -35,6 +37,35 @@ def executar_decifrar(texto_binario: str, chave: str) -> Decifrar_Response:
             texto = ''
 
     return Decifrar_Response(texto_claro=texto_claro)
+
+async def executar_decifrar_forca_bruta(texto_cifrado: str) -> Decifrar_Forca_Bruta_Response:
+    resposta = await buscar_palavra(texto_cifrado)
+    return Decifrar_Forca_Bruta_Response(textoClaro=resposta)
+
+async def buscar_palavra(palavra: str):
+    url = f"https://api.dicionario-aberto.net/word/{palavra}"
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resposta:
+            if resposta.status != 200:
+                return f"Erro: Falha ao consultar API (status {resposta.status})"
+
+            dados = await resposta.json()
+
+            print(dados)
+
+            if isinstance(dados, list) and len(dados) > 0 and "xml" in dados[0]:
+                xml_texto = dados[0]["xml"]
+
+                definicoes = re.findall(r"<orth>(.*?)</orth>", xml_texto, re.DOTALL)
+
+                if definicoes:
+                    definicao_limpa = re.sub(r"<.*?>", "", definicoes[0]).strip()
+                    return definicao_limpa
+
+                return "Definição não encontrada no XML."
+            else:
+                return "Nenhuma definição encontrada."
 
 def entrada_valida(chave_binario: str, texto_binario: str, mensagemErro: str) -> HTTPException:
     if len(chave_binario) < len(texto_binario):
